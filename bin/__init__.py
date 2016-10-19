98d32c79ba6e906fbd62462a70ff260ce4069f2a
kongbluz <57011501@kmitl.ac.th>
2016-10-19 22:01:54 +0700

    2.7

--- bin/__init__.py
+++ bin/__init__.py
@@ -3,7 +3,7 @@ from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from .models.models  import DBSession, Base
+from models.models  import DBSession, Base
 
 def main(global_config, **settings):
 
