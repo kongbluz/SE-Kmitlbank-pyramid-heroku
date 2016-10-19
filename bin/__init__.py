6f8546b9edc16ab0b0490e253beefd287f2fe0d9
Kong Atthasit <57011501@kmitl.ac.th>
2016-10-19 22:17:03 +0700

    Update __init__.py

--- bin/__init__.py
+++ bin/__init__.py
@@ -4,7 +4,7 @@ from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
 import sys
-sys.path.append('/models')
+path.append(getcwd() + "\\models")
 from models.models  import DBSession, Base
 
 def main(global_config, **settings):
