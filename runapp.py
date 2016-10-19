79f570f464c0f09a8c5d9c4829ea704a2c64d272
kongbluz <57011501@kmitl.ac.th>
2016-10-19 23:55:26 +0700

    new version

--- runapp.py
+++ runapp.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
 
-    serve(app, host='0.0.0.0', port=port, numthreads=10, request_queue_size=200)
+    serve(app, host='49.49.234.203', port=port, numthreads=10, request_queue_size=200)
