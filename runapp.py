f2bf1b04b22ecb1dcd9f44601da812bc70934913
kongbluz <57011501@kmitl.ac.th>
2016-10-19 23:49:47 +0700

    new version

--- runapp.py
+++ runapp.py
@@ -4,7 +4,7 @@ from paste.deploy import loadapp
 from waitress import serve
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
 
-    serve(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port, numthreads=10, request_queue_size=200)
