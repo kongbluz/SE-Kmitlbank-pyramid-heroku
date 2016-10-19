96733a385443dbfb828933481f20cc86528da06f
kongbluz <57011501@kmitl.ac.th>
2016-10-19 21:08:26 +0700

    second

--- runapp.py
+++ runapp.py
@@ -5,4 +5,6 @@ from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app = loadapp('config:production.ini', relative_to='.')
+    app = loadapp('production.ini', relative_to='.')
+
+    serve(app, host='0.0.0.0', port=port)
