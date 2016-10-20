11e389bbda8c93a44a4be1267cba7bbb263a773e
kongbluz <57011501@kmitl.ac.th>
2016-10-21 01:55:09 +0700

    edit js

--- bin/static/js/accountsubmit.js
+++ bin/static/js/accountsubmit.js
@@ -1,17 +1,17 @@
 function fncSubmit() {
-  if(document.form1.FrmIdcn.value == "" || isNaN(this.value) ) {
+  if(document.form1.nationid.value == "" || isNaN(this.value) ) {
     alert('Please input Nation ID and input ');
-    document.form1.FrmIdcn.focus();
+    document.form1.nationid.focus();
     return false;
   }
-  else if(document.form1.phone.value == "" || form1.phone.length != 10)  {
+  else if(document.form1.phonenumber.value == "" || form1.phonenumber.length != 10)  {
     alert('Please input phone number');
-    document.form1.phone.focus();
+    document.form1.phonenumber.focus();
     return false;
   }
-  else if(document.form1.txtName.value == "")  {
+  else if(document.form1.name.value == "")  {
     alert('Please input Name');
-    document.form1.txtName.focus();
+    document.form1.name.focus();
     return false;
   }
   document.form1.submit();
--- bin/static/js/regissubmit.js
+++ bin/static/js/regissubmit.js
@@ -1,12 +1,12 @@
 function fncSubmit() {
-  if(document.form1.txtUsername.value == "" || form1.txtUsername.value.length < 6 || form1.txtUsername.value.length > 12){
+  if(document.form1.username.value == "" || form1.username.value.length < 6 || form1.username.value.length > 12){
     alert('Please input Username and have 6 - 12 characters');
-    document.form1.txtUsername.focus();
+    document.form1.username.focus();
     return false;
   }
-  else if(document.form1.txtPassword.value == "")  {
+  else if(document.form1.password.value == "")  {
     alert('Please input Password');
-    document.form1.txtPassword.focus();
+    document.form1.password.focus();
     return false;
   }
   else if(document.form1.txtConPassword.value == "")  {
@@ -14,19 +14,19 @@ function fncSubmit() {
     document.form1.txtConPassword.focus();
     return false;
   }
-  else if(document.form1.txtPassword.value != document.form1.txtConPassword.value)  {
+  else if(document.form1.password.value != document.form1.txtConPassword.value)  {
     alert('Confirm Password Not Match');
     document.form1.txtConPassword.focus();
     return false;
   }
   else if(document.form1.txtName.value == "")  {
     alert('Please input Account Name');
-    document.form1.txtName.focus();
+    document.form1.accountname.focus();
     return false;
   }
-  else if(document.form1.txtNum.value == "")  {
+  else if(document.form1.accountid.value == "")  {
     alert('Please input Account Num');
-    document.form1.txtName.focus();
+    document.form1.accountid.focus();
     return false;
   }
   document.form1.submit();
