a4602809a5b3d7b84adf9a71c551b540134b6d42
kongbluz <57011501@kmitl.ac.th>
2016-10-21 02:24:02 +0700

    edit js

--- bin/static/js/regissubmit.js
+++ bin/static/js/regissubmit.js
@@ -1,30 +1,30 @@
 function fncSubmit() {
-  if(document.form1.txtusername.value == "" || form1.txtusername.value.length < 6 || form1.txtusername.value.length > 12){
+  if(document.form1.username.value == "" || form1.username.value.length < 6 || form1.username.value.length > 12){
     alert('Please input Username and have 6 - 12 characters');
     document.form1.username.focus();
     return false;
   }
-  else if(document.form1.txtpassword.value == "")  {
+  else if(document.form1.password.value == "")  {
     alert('Please input Password');
     document.form1.password.focus();
     return false;
   }
-  else if(document.form1.txtConPassword.value == "")  {
+  else if(document.form1.ConPassword.value == "")  {
     alert('Please input Confirm Password');
     document.form1.txtConPassword.focus();
     return false;
   }
-  else if(document.form1.txtpassword.value != document.form1.txtConPassword.value)  {
+  else if(document.form1.password.value != document.form1.txtConPassword.value)  {
     alert('Confirm Password Not Match');
     document.form1.txtConPassword.focus();
     return false;
   }
-  else if(document.form1.txtName.value == "")  {
+  else if(document.form1.accountname.value == "")  {
     alert('Please input Account Name');
     document.form1.accountname.focus();
     return false;
   }
-  else if(document.form1.txtaccountid.value == "")  {
+  else if(document.form1.accountid.value == "")  {
     alert('Please input Account Num');
     document.form1.accountid.focus();
     return false;
