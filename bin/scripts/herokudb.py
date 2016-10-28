32bd397c232cbe37ca2c2535d5929ea5e0197cda
kongbluz <57011501@kmitl.ac.th>
2016-10-29 03:11:12 +0700

    for herokuapp

---
+++ bin/scripts/herokudb.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import transaction
+
+from ..models.models import (
+    DBSession,
+    Base,
+    UserAccount,
+    OwnerBankaccount,
+    Transaction,
+    Costumer,
+    Forum,
+    BankAccount,
+    RepeatPayment,
+    )
+
+def main(argv=sys.argv):
+    settings = {'sqlalchemy.url': os.environ['DATABASE_URL']}
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    with transaction.manager:
+        modelcostumer = Costumer(nationid = '1001', fullname = 'admin eieigum', brithday = datetime.datetime.now(), address = 'guy u u u u u u', phonenumber = '098765434312')
+        DBSession.add(modelcostumer)
+        modelaccount = BankAccount(accountname = 'adminnaja', Costumer_id = 1)
+        DBSession.add(modelaccount)
+        modeluser = UserAccount(username='admin', password = hash_password('1234'),role = 'editor', Costumer_id = 1)
+        DBSession.add(modeluser)
+        modelob   = OwnerBankaccount(userid = 1, bankid = 1)
+        DBSession.add(modelob)
+        modelsforum = Forum(title='root', detail='root', createdtime = datetime.datetime.now())
+        DBSession.add(modelsforum)
+
+        modelcostumer = Costumer(nationid = '0', fullname = 'government', brithday = datetime.datetime.now(), address = 'on the world', phonenumber = '0000000000')
+        DBSession.add(modelcostumer)
+        modelaccount = BankAccount(accountname = 'government', Costumer_id = 2)
+        DBSession.add(modelaccount)
+        modeluser = UserAccount(username='topsecert', password = hash_password('topsecert'), role = 'veiwer', Costumer_id = 2)
+        DBSession.add(modeluser)
+        modelob = OwnerBankaccount(userid = 2, bankid = 2)
+        DBSession.add(modelob)
+
+
+if __name__ == '__main__':
+    main()
--- runapp.py
+++ runapp.py
@@ -2,10 +2,13 @@ import os
 
 from paste.deploy import loadapp
 from waitress import serve
+import bin
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', '8080'))
     host = '0.0.0.0'
-    app = loadapp('config:development.ini', relative_to='.')
+    settings = {}
+    settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
+    settings['pyramid.includes'] = ['pyramid_tm']
 
-    serve(app, host=host, port=port)
+    serve(bin.main({}, **settings), host=host, port=port)
