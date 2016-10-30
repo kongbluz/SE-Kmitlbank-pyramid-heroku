c1664f6f5c46a540ea727bd9345d82ae2620f44f
kongbluz <57011501@kmitl.ac.th>
2016-10-30 20:15:51 +0700

    add default db

--- bin/scripts/herokudb.py
+++ bin/scripts/herokudb.py
@@ -25,7 +25,16 @@ def main(argv=sys.argv):
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
 
-    
+    with transaction.manager:
+        modelcostumer = Costumer(nationid = '1001', fullname = 'admin eieigum', brithday = datetime.datetime.now(), address = 'guy u u u u u u', phonenumber = '098765434312')
+        DBSession.add(modelcostumer)
+        modelaccount = BankAccount(accountname = 'adminnaja', Costumer_id = 1)
+        DBSession.add(modelaccount)
+        modeluser = UserAccount(username='admin', password = hash_password('1234'),role = 'editor', Costumer_id = 1)
+        DBSession.add(modeluser)
+        modelob   = OwnerBankaccount(userid = 1, bankid = 1)
+        DBSession.add(modelob)
+
 
 if __name__ == '__main__':
     main()
