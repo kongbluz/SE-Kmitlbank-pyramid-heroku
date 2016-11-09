450be2aa3164e41f9fe41b4e413b145d6c690cba
kongbluz <57011501@kmitl.ac.th>
2016-11-09 23:55:21 +0700

    improve beau

--- bin/static/js/accountsubmit.js
+++ bin/static/js/accountsubmit.js
@@ -1,11 +1,11 @@
 function fncSubmit() {
-  if(document.form1.nationid.value == "") {
-    alert('Please input Nation ID ');
+  if(document.form1.nationid.value.length < 13) {
+    alert('Nation ID is 13 characters');
     document.form1.nationid.focus();
     return false;
   }
-  else if(document.form1.phonenumber.value == "")  {
-    alert('Please input phone number');
+  else if(document.form1.phonenumber.value.length < 9)  {
+    alert('Phone number is between 9 - 10 characters');
     document.form1.phonenumber.focus();
     return false;
   }
